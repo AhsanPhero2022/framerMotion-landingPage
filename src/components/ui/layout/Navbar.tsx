da84fb123f9ed3f74dddd307c95af053d8bc794f
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-01-26 20:45:41 +0600

    header section done with animation

--- src/components/ui/layout/Navbar.tsx
+++ src/components/ui/layout/Navbar.tsx
@@ -1,9 +1,14 @@
 import { NavLink } from "react-router-dom";
 import { Button } from "../button";
-
+import { motion } from "framer-motion";
 const Navbar = () => {
   return (
-    <header className=" h-[40px] mt-4">
+    <motion.header
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2 }}
+      className=" h-[40px] mt-4"
+    >
       <nav className=" w-full max-w-[1230px] mx-auto px-[20px] flex justify-between h-full items-center">
         <span className="text-2xl font-semibold">iRepair</span>
         <ul className="space-x-5">
@@ -14,7 +19,7 @@ const Navbar = () => {
           </Button>
         </ul>
       </nav>
-    </header>
+    </motion.header>
   );
 };
 
--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -1,28 +1,86 @@
 import Container from "@/components/ui/Container";
 import { Button } from "@/components/ui/button";
-
+import { motion } from "framer-motion";
 const HeroSection = () => {
-  return (
-    <Container className="h-[calc(100vh-64px)] grid grid-cols-2 place-content-center">
-      <div>
-        <h1 className="text-8xl font-bold">
-          <span className="text-gray">Don't worry</span>
-          <br />
-          <span>We'll fix it</span>
-        </h1>
-        <p>
-          Welcome to <span>iRepair</span>, your one-stop place for all kinds of
-          <span>Mackbook repairs</span> and diagnostics
-        </p>
-        <Button>Book a service</Button>
-      </div>
+  const intro = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        staggerChildren: 0.25,
+        delayChildren: 0.5,
+      },
+    },
+  };
 
-      <div>
+  const introChildren = {
+    hidden: { opacity: 0, y: -200 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.7, type: "spring", bounce: 0.5 },
+    },
+  };
+
+  const laptop = {
+    initial: { y: 0, scale: 4 },
+    animate: {
+      y: 20,
+      scale: 1,
+      rotate: -30,
+      transition: {
+        duration: 1,
+        y: {
+          duration: 2,
+          repeat: Infinity,
+          repeatType: "reverse",
+          ease: "easeInOut",
+        },
+      },
+    },
+  };
+
+  return (
+    <Container className="h-[calc(100vh-64px)] grid lg:grid-cols-2 place-content-center overflow-hidden">
+      <motion.div variants={intro} animate="visible" initial="hidden">
+        <motion.h1
+          variants={introChildren}
+          className="text-5xl lg:text-8xl font-bold text-nowrap"
+        >
+          <span className="text-gray">Don't worry,</span>
+          <br />
+          <span>We'll fix it.</span>
+        </motion.h1>
+        <motion.p
+          variants={introChildren}
+          className="mt-10 mb-5 text-dark-gray text-lg max-w-[50ch]"
+        >
+          Welcome to
+          <span className="text-primary-foreground font-semibold">iRepair</span>
+          , your one-stop place for all kinds of
+          <span className="text-primary-foreground  font-semibold">
+            Mackbook repairs
+          </span>
+          and diagnostics
+        </motion.p>
+        <motion.div variants={introChildren}>
+          <Button>Book a service</Button>
+        </motion.div>
+      </motion.div>
+
+      <motion.div
+        variants={laptop}
+        initial="initial"
+        animate="animate"
+        className="mt-10 mx-auto w-full "
+      >
         <img
+          className=" rounded-md mx-auto h-[90%] object-contain"
           src="https://5.imimg.com/data5/SELLER/Default/2023/6/316317115/VU/LP/UY/181126883/macbook-repair-services.jpg"
           alt=""
         />
-      </div>
+      </motion.div>
     </Container>
   );
 };
